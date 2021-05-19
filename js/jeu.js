a9d3e9039eeab9ea7acc9e4ce8bb8b71397488c2
aero309 <81588796+aero309@users.noreply.github.com>
2021-05-19 23:43:34 +0200

    Add files via upload

--- js/jeu.js
+++ js/jeu.js
@@ -1,25 +1,38 @@
 //////////////////////////////////////
-/*Perso*/
-//////////////////////////////////////
-
-
-function Rage() {
-  var perso = "./img/PepeRage.jpg"
-}
-
-function Poggers() {
-  var perso = "./img/poggers.jpg"
-}
-
-//////////////////////////////////////
 /*Jeu*/
 //////////////////////////////////////
 
-var balance = 0
+// Definir le nombre de coins au debut du jeu
+var balance = 0;
 
-
-
+// Initialiser le compteur avec un increment parametreable
 function ajout(x) {
-  balance += x
-  document.getElementById("balance").innerHTML = balance
+  balance += x;
+  document.getElementById("balance").innerHTML = balance;
+}
+
+// Definir la valeur initiale de l'increment
+var ini = 1;
+
+// Evenement onclick pour lancer le compteur et d'afficher le nombre de coins (var balance)
+document.getElementById("main-button").onclick = function() {ajout(ini)};
+
+// Definir la valeur initiale du prix pour "Upgrade coins per click"
+var price_clicks = 100;
+
+// Afficher le prix initial du "Upgrade coins per click"
+document.getElementById("price_clicks").innerHTML = "Price:" + " " + price_clicks;
+
+// Initialiser la fonction qui effectue l'achat
+function increase_click(){
+  if (balance >= price_clicks) {
+      balance -= price_clicks;
+      document.getElementById("balance").innerHTML = balance;
+      ini += 1;
+      document.getElementById("main-button").onclick = function() {ajout(ini)}
+      price_clicks *= 2
+      document.getElementById("price_clicks").innerHTML = "Price:" + " " + price_clicks;
+  } else {
+    alert("Il vous faut au moins" + " " + price_clicks + " " + "Pepe Coins")
+  }
 }
