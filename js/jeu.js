c62c6dc55761e42529ed8a3f9387771959582e06
aero309 <81588796+aero309@users.noreply.github.com>
2021-05-22 12:22:49 +0200

    Add files via upload

--- js/jeu.js
+++ js/jeu.js
@@ -1,12 +1,16 @@
 //////////////////////////////////////
+/*Animation*/
+//////////////////////////////////////
+
+//////////////////////////////////////
 /*Jeu*/
 //////////////////////////////////////
 
 // Definir le nombre de coins au debut du jeu
 var balance = 0;
 
-
-
+// Afficher le nombre de coins au debut du jeu
+document.getElementById("balance").innerHTML = balance;
 
 // Initialiser le compteur avec un increment parametreable
 function ajout(x) {
@@ -18,54 +22,57 @@ function ajout(x) {
 var ini_click = 1;
 
 // Evenement onclick pour lancer le compteur et d'afficher le nombre de coins (var balance)
-document.getElementById("main-button").onclick = function() {ajout(ini_click)};
+document.getElementById("main-button").onclick = function () {
+  ajout(ini_click);
+};
 
 // Definir la valeur initiale du prix pour "Upgrade coins per click"
 var price_clicks = 100;
 
 // Afficher le prix initial du "Upgrade coins per click"
-document.getElementById("price_clicks").innerHTML = "Price:" + " " + price_clicks;
+document.getElementById("price_clicks").innerHTML =
+  "Price:" + " " + price_clicks;
 
 // Initialiser la fonction qui effectue l'achat
-function increase_click(){
+function increase_click() {
   if (balance >= price_clicks) {
     balance -= price_clicks;
     document.getElementById("balance").innerHTML = balance;
     ini_click += 1;
-    document.getElementById("main-button").onclick = function() {ajout(ini_click)}
-    price_clicks *= 2
-    document.getElementById("price_clicks").innerHTML = "Price:" + " " + price_clicks;
+    document.getElementById("main-button").onclick = function () {
+      ajout(ini_click);
+    };
+    price_clicks *= 2;
+    document.getElementById("price_clicks").innerHTML =
+      "Price:" + " " + price_clicks;
   } else {
-    alert("Il vous faut au moins" + " " + price_clicks + " " + "Pepe Coins")
+    alert("Il vous faut au moins" + " " + price_clicks + " " + "Pepe Coins");
   }
 }
-
-
 
 var price_auto = 200;
 var ini_auto = 1000;
 
 document.getElementById("price_auto").innerHTML = "Price:" + " " + price_auto;
 
-
 function autoclick() {
   document.getElementById("main-button").click();
 }
 
-
-function upgrade_auto(){
+function upgrade_auto() {
   if (balance >= price_auto) {
-      if (ini_auto >= 300){
-        balance -= price_auto;
-        document.getElementById("balance").innerHTML = balance;
-        ini_auto -= 100;
-        setInterval(autoclick, ini_auto);
-        price_auto *=2 ;
-        document.getElementById("price_auto").innerHTML = "Price:" + " " + price_auto;
-      } else {
-        alert("Vous avez atteint l'auto-clique le plus rapide du jeu!")
-      }
+    if (ini_auto >= 300) {
+      balance -= price_auto;
+      document.getElementById("balance").innerHTML = balance;
+      ini_auto -= 100;
+      setInterval(autoclick, ini_auto);
+      price_auto *= 2;
+      document.getElementById("price_auto").innerHTML =
+        "Price:" + " " + price_auto;
+    } else {
+      alert("Vous avez atteint l'auto-clique le plus rapide du jeu!");
+    }
   } else {
-    alert("Il vous faut au moins" + " " + price_auto + " " + "Pepe Coins")
+    alert("Il vous faut au moins" + " " + price_auto + " " + "Pepe Coins");
   }
 }
